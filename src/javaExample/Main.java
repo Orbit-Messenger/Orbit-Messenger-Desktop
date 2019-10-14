015084aaa927da6db1fc672de255525b0cb50ba9
MaxwellM <maxwellrmorin@gmail.com>
2019-10-14 13:28:31 -0600

    More UI Changes.

--- src/javaExample/Main.java
+++ src/javaExample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Messaging Example");
+        primaryStage.setTitle("Orbit Messenger");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
