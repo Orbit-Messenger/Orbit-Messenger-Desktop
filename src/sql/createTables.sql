7116d060fa5977b9752b500d7d522c8be6900e34
brody prestwich <brody.prestwich18@gmail.com>
2019-11-17 11:43:21 -0700

    added chatrooms to the database

--- src/sql/createTables.sql
+++ src/sql/createTables.sql
@@ -12,14 +12,22 @@ INSERT INTO users VALUES(DEFAULT, 'brody', 'test', 'test', 'admin');
 INSERT INTO users VALUES(DEFAULT, 'admin', 'test', 'test', 'admin');
 INSERT INTO users VALUES(DEFAULT, 'test', 'test', 'test', 'user');
 
+CREATE TABLE IF NOT EXISTS chatrooms(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(50) NOT NULL
+);
+
+INSERT INTO chatrooms VALUES(DEFAULT, 'general');
+
 CREATE TABLE IF NOT EXISTS messages(
   id SERIAL PRIMARY KEY,
   user_id INT REFERENCES users(id),
+  chatroom_id INT REFERENCES chatrooms(id),
   message TEXT,
   time_stamp timestamp
 );
 
-INSERT INTO messages VALUES(DEFAULT, 1, 'testing 1', 'now');
-INSERT INTO messages VALUES(DEFAULT, 2, 'testing 2', 'now');
-INSERT INTO messages VALUES(DEFAULT, 3, 'testing 3', 'now');
-INSERT INTO messages VALUES(DEFAULT, 4, 'testing 4', 'now');
+INSERT INTO messages VALUES(DEFAULT, 1, 1, 'testing 1', 'now');
+INSERT INTO messages VALUES(DEFAULT, 2, 1, 'testing 2', 'now');
+INSERT INTO messages VALUES(DEFAULT, 3, 1, 'testing 3', 'now');
+INSERT INTO messages VALUES(DEFAULT, 4, 1, 'testing 4', 'now');
--- src/sql/dropTables.sql
+++ src/sql/dropTables.sql
@@ -1,2 +1,3 @@
 DROP TABLE IF EXISTS messages;
+DROP TABLE IF EXISTS chatrooms;
 DROP TABLE IF EXISTS users;
